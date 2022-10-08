e6f1f8a9c08fccccd89858975318aab26ff09513
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-10-08 12:11:17 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Justin Hold on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
